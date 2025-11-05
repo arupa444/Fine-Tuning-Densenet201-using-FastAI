d82350a4444485972c69e8c8557a72f99b208908
arupanandaswain <arupaswain7735@gmail.com>
2025-11-06 04:44:04 +0530

    updated requirements.txt and app.py

--- fastAPI/app.py
+++ fastAPI/app.py
@@ -9,15 +9,14 @@ import onnxruntime as ort
 
 app = FastAPI()
 
-
 # Load YOLO TFLite models for object detection
 @lru_cache(maxsize=1)
 def get_crate_model():
-    return YOLO("model/jfl_crate_model_obb_01052025.tflite", task="obb")
+    return YOLO("model/jfl_crate_model_obb_01052025_with_nms_conf_0.5_yolo_v11.tflite", task="obb")
 
 @lru_cache(maxsize=1)
 def get_marker_model():
-    return YOLO("model/jfl_marker_vertical_rotate_28_july_2025.tflite", task="obb")
+    return YOLO("jfl_marker_vertical_rotate_28_july_2025_float32_nms_false_yolo_v11.tflite", task="obb")
 
 
 SUPPORTED_IMAGE_TYPES = [
